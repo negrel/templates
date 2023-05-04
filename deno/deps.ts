e7b75bdea012b7f688e5f23967a930bc4794aba6
Alexandre Negrel <negrel.dev@protonmail.com>
2023-05-04 22:20:02 +0200

    add deno template

---
+++ deno/deps.ts
@@ -0,0 +1,1 @@
+export { getLogger } from "https://deno.land/std@$DENO_STDLIB_VERSION/log/mod.ts";
--- flake.nix
+++ flake.nix
@@ -1,19 +1,38 @@
 {
   outputs = { self, nixpkgs }: {
-    templates = rec {
-      default = {
-        path = ./flake;
-        description = "A simple Nix flake";
+    templates =
+      let commonEnvVars = {
+        # OPTION_NAME = DEFAULT_VALUE
+        PROJECT_NAME = "";
+        DESCRIPTION = "";
+        LONG_DESCRIPTION = "";
       };
-      flake = default;
-      rust = {
-        path = ./rust;
-        description = "A simple Rust project";
-        envVars = {
-          PKG = "rust";
-          EDITION = 2021;
+      in
+      rec {
+        default = {
+          path = ./flake;
+          description = "A simple Nix flake";
+          envVars = commonEnvVars;
+        };
+        flake = default;
+        rust = {
+          path = ./rust;
+          description = "A simple Rust project";
+          envVars = commonEnvVars //
+            {
+              # Package name in Cargo.toml
+              PKG = "rust";
+              # Rust edition entry in Cargo.toml
+              EDITION = 2021;
+            };
+        };
+        deno = {
+          path = ./deno;
+          description = "A simple Deno project";
+          envVars = commonEnvVars // {
+            DENO_STDLIB_VERSION = "0.186.0";
+          };
         };
       };
-    };
   };
 }
