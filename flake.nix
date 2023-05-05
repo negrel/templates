5023b980ddc5b034fb6e9f81dad8c1d1fdf7e3bb
Alexandre Negrel <negrel.dev@protonmail.com>
2023-05-05 09:59:08 +0200

    add k6 template

--- flake.nix
+++ flake.nix
@@ -14,7 +14,21 @@
           description = "A simple Nix flake";
           envVars = commonEnvVars;
         };
+        deno = {
+          path = ./deno;
+          description = "A simple Deno project";
+          envVars = commonEnvVars // {
+            DENO_STDLIB_VERSION = "0.186.0";
+          };
+        };
         flake = default;
+        k6 = {
+          path = ./k6;
+          description = "A ready to use k6 setup";
+          envVars = commonEnvVars // {
+            K6_VERSION = "latest";
+          };
+        };
         rust = {
           path = ./rust;
           description = "A simple Rust project";
@@ -25,13 +39,6 @@
               # Rust edition entry in Cargo.toml
               EDITION = 2021;
             };
-        };
-        deno = {
-          path = ./deno;
-          description = "A simple Deno project";
-          envVars = commonEnvVars // {
-            DENO_STDLIB_VERSION = "0.186.0";
-          };
         };
       };
   };
