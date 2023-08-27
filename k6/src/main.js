9881a6d643d5e6be1d73322e26b981415af0c9fc
Alexandre Negrel <negrel.dev@protonmail.com>
2023-08-27 16:48:56 +0200

    update k6 template

---
+++ k6/src/main.js
@@ -0,0 +1,26 @@
+/* __ENV */
+
+/**
+ * Test options.
+ * https://k6.io/docs/using-k6/k6-options/
+ */
+export const options = {}
+
+/**
+ * Setup function before VUs iterations.
+ * @link https://k6.io/docs/using-k6/test-lifecycle/
+ */
+export function setup () {}
+
+/**
+ * Virtual User iteration.
+ * @link https://k6.io/docs/using-k6/test-lifecycle/
+ */
+export default function () {}
+
+/**
+ * Tear down tests.
+ * @link https://k6.io/docs/using-k6/test-lifecycle/
+ */
+export function teardown () {}
+
