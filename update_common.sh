fbe8e48187d6ee76e5100e5742078db3c3a7ae8e
Alexandre Negrel <alexandre@negrel.dev>
2024-01-04 14:13:42 +0100

    update common files

--- update_common.sh
+++ update_common.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
 if [ "$(git diff | wc -l)" != "0" ]; then
-  echo "commit your change before updating common files" >&2
-  exit 1
+	echo "commit your change before updating common files" >&2
+	exit 1
 fi
 
 for template in *; do
-  if [ -f "$template" ]; then continue; fi
+	if [ -f "$template" ]; then continue; fi
 
-  echo "updating $template template..."
+	echo "updating $template template..."
 
-  pushd "$template"
+	pushd "$template"
 
-  while IFS= read -r -d '' file; do
-    copied_file="${file#../.common/}"
+	while IFS= read -r -d '' file; do
+		copied_file="${file#../.common/}"
 
 		# If file already exist
 		if [ -f "$copied_file" ]; then
-		# And is different that current common file
-		# we ignore it.
+			# And is different that current common file
+			# we ignore it.
 			diff "$file" "$copied_file" || continue
 		fi
 
-    mkdir -p $(dirname "$copied_file")
-    ln -f "$file" "$copied_file"
-  done < <(find "../.common" -type f -print0)
+		mkdir -p $(dirname "$copied_file")
+		ln -f "$file" "$copied_file"
+	done < <(find "../.common" -type f -print0)
 
-  popd
+	popd
 
-  echo "$template template updated."
+	echo "$template template updated."
 done
